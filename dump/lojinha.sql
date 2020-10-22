b90c9bab7a2d450821564ba2c6c00cfb786ea514
unknown <30684@fiecalunos.lan>
2020-10-21 22:41:45 -0300

    ...

--- dump/lojinha.sql
+++ dump/lojinha.sql
@@ -13,12 +13,10 @@
 
 
 -- Copiando estrutura do banco de dados para lojinha
-DROP DATABASE IF EXISTS `lojinha`;
 CREATE DATABASE IF NOT EXISTS `lojinha` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `lojinha`;
 
 -- Copiando estrutura para tabela lojinha.t_produtos
-DROP TABLE IF EXISTS `t_produtos`;
 CREATE TABLE IF NOT EXISTS `t_produtos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) DEFAULT NULL,
@@ -27,12 +25,15 @@ CREATE TABLE IF NOT EXISTS `t_produtos` (
   `qtdecoletiva` int(11) DEFAULT NULL,
   `precocoletivo` float DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela lojinha.t_produtos: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `t_produtos` DISABLE KEYS */;
+INSERT INTO `t_produtos` (`id`, `nome`, `foto`, `preco`, `qtdecoletiva`, `precocoletivo`) VALUES
+	(1, 'teste', 'https://d26lpennugtm8s.cloudfront.net/stores/938/754/products/oak0111-b435eb9a5e42eb989f15596819819516-640-0.png', 10.9, NULL, NULL);
+/*!40000 ALTER TABLE `t_produtos` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela lojinha.t_usuarios
-DROP TABLE IF EXISTS `t_usuarios`;
 CREATE TABLE IF NOT EXISTS `t_usuarios` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) DEFAULT NULL,
@@ -40,14 +41,19 @@ CREATE TABLE IF NOT EXISTS `t_usuarios` (
   `cep` varchar(9) DEFAULT NULL,
   `numero` int(11) DEFAULT NULL,
   `complemento` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela lojinha.t_usuarios: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `t_usuarios` DISABLE KEYS */;
+INSERT INTO `t_usuarios` (`id`, `email`, `senha`, `cep`, `numero`, `complemento`, `nome`) VALUES
+	(1, 'cassiel@gmail.com', 'admin123', NULL, NULL, NULL, NULL),
+	(2, 'teste@gmail.com', '123', NULL, NULL, NULL, 'cassiel');
+/*!40000 ALTER TABLE `t_usuarios` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela lojinha.v_compras
-DROP TABLE IF EXISTS `v_compras`;
 CREATE TABLE IF NOT EXISTS `v_compras` (
   `id_usuario` int(11) DEFAULT NULL,
   `id_produto` int(11) DEFAULT NULL,
@@ -58,7 +64,9 @@ CREATE TABLE IF NOT EXISTS `v_compras` (
   CONSTRAINT `v_compras_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `t_produtos` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela lojinha.v_compras: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `v_compras` DISABLE KEYS */;
+/*!40000 ALTER TABLE `v_compras` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
