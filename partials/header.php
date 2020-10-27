7e52d4f33f324a068d20090e0838bf6273e7880c
unknown <30684@fiecalunos.lan>
2020-10-26 22:27:16 -0300

    ...

--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$url = $_SERVER['PHP_SELF'];
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +18,7 @@ session_start();
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#"> <?php if (isset($_SESSION['meu_nome'])) ?><?= $_SESSION['meu_nome'] ?></a>
+        <a class="navbar-brand" href="#"> <?php  if (isset($_SESSION['meu_nome'])) echo $_SESSION['meu_nome'] ?></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
